53f62866e529146fd969d8eb2dea3c9fec59c7e0
Matthias Kalms <mtkalms@gmail.com>
2025-08-01 17:39:52 +0000

    Add sinatra frontend

--- controllers/blogs_controller.rb
+++ controllers/blogs_controller.rb
@@ -3,7 +3,39 @@ get '/blogs' do
   @blogs.to_json
 end
 
+post '/blogs' do
+  @blog = Blog.create(
+    name: params[:name], 
+    tagline: params[:tagline]
+  ) 
+  if @blog.save
+    status 201
+    redirect "/blogs/#{@blog.id}/view"
+  else
+    status 422
+    { error: 'Failed to create blog' }.to_json
+  end
+end
+
+get '/blogs/view' do
+  content_type :html
+  @blogs = Blog.all
+  erb :blogs
+end
+
+get '/blogs/new' do
+  content_type :html
+  erb :blogs_new
+end
+
 get '/blogs/:id' do
   @blog = Blog.find_by_id(params[:id])
   @blog.to_json
+end
+
+get '/blogs/:id/view' do
+  content_type :html
+  @blog = Blog.find_by_id(params[:id])
+  @posts = @blog.posts
+  erb :blogs_view
 end
--- controllers/posts_controller.rb
+++ controllers/posts_controller.rb
@@ -4,7 +4,55 @@ get '/blogs/:blog_id/posts' do
   @posts.to_json
 end
 
+post '/blogs/:blog_id/posts' do
+  @post = Post.create(
+    blog_id: params[:blog_id],
+    headline: params[:headline],
+    body_text: params[:body_text],  
+  )
+  if @post.save
+    status 201
+    redirect "/blogs/#{params[:blog_id]}/posts/#{@post.id}/view"
+  else
+    status 422
+    { error: 'Failed to create post' }.to_json
+  end
+end
+
 get '/posts/:id' do
   @post = Post.find_by_id(params[:id])
   @post.to_json
+end
+
+get '/posts/:id/view' do
+  content_type :html
+  @post = Post.find_by_id(params[:id])
+  if @post.nil?
+    halt 404, { error: 'Post not found' }.to_json
+  end
+  @blog = Blog.find_by_id(@post.blog_id)
+  erb :posts_view
+end
+
+get '/blogs/:blog_id/posts/view' do
+  content_type :html
+  @blog = Blog.find_by_id(params[:blog_id])
+  @posts = @blog.posts
+  erb :posts
+end
+
+get '/blogs/:blog_id/posts/:id/view' do
+  content_type :html
+  @post = Post.find_by_id(params[:id])
+  if @post.nil? or @post.blog_id != params[:blog_id].to_i
+    halt 404, { error: 'Post not found in this blog' }.to_json
+  end
+  @blog = Blog.find_by_id(@post.blog_id)
+  erb :posts_view
+end
+
+get '/blogs/:blog_id/posts/new' do
+  content_type :html
+  @blog = Blog.find_by_id(params[:blog_id])
+  erb :posts_new
 end
