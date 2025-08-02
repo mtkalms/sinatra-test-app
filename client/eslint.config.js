2258f3d49e839906ea2579da3b61a2655f82e154
Matthias Kalms <mtkalms@gmail.com>
2025-08-02 17:32:43 +0000

    Add prettier formatter

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import prettier from "eslint-plugin-prettier";
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -18,6 +19,18 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: prettier,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
   },
 ])
