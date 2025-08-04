dd3f0a554ba629acfdd0465302dbbf0093cc385e
Matthias Kalms <mtkalms@gmail.com>
2025-08-04 17:00:09 +0000

    Sanitize html content

--- client/src/pages/sinatra.tsx
+++ client/src/pages/sinatra.tsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router";
 import useSinatra from "../hooks/useSinatra";
 import { Gem } from "lucide-react";
+import DOMPurify from "dompurify";
 
 export default function SinatraPage() {
   const location = useLocation();
@@ -8,7 +9,11 @@ export default function SinatraPage() {
     "http://localhost:4567" + location.pathname,
   );
 
-  console.log(data);
+  function sanitize(html: string): string {
+    return DOMPurify.sanitize(html) // Sanitize the HTML to prevent XSS attacks
+      .replace('action="', 'action="http://localhost:4567'); // Ensure the action attribute points to the frontend server
+  }
+
   return (
     <div>
       <div className="flex gap-2 bg-red-500 p-2 align-middle text-white">
@@ -19,10 +24,7 @@ export default function SinatraPage() {
       {data && (
         <div
           dangerouslySetInnerHTML={{
-            __html: data?.replace(
-              'action="',
-              'action="http://localhost:4567',
-            ) as string,
+            __html: sanitize(data),
           }}
         />
       )}
