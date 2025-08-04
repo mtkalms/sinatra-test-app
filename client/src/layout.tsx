f93a4ba1005533cf7b59c37f589b55a99dbf5f72
Matthias Kalms <mtkalms@gmail.com>
2025-08-04 16:44:11 +0000

    Add blogs to sidebar

--- client/src/layout.tsx
+++ client/src/layout.tsx
@@ -2,6 +2,10 @@ import { Link, Outlet } from "react-router";
 import {
   Sidebar,
   SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
   SidebarInset,
   SidebarMenu,
   SidebarMenuButton,
@@ -11,22 +15,63 @@ import {
 } from "./components/ui/sidebar";
 import { Separator } from "@radix-ui/react-separator";
 import { ModeToggle } from "./components/mode-toggle";
-import { BookText } from "lucide-react";
+import { BookOpen, ChevronRight } from "lucide-react";
+import useFetch from "./hooks/useFetch";
+import { Collapsible, CollapsibleTrigger } from "./components/ui/collapsible";
+import { CollapsibleContent } from "@radix-ui/react-collapsible";
 
 export default function Layout() {
+  const { data: blogs } = useFetch<Blog[]>(`http://localhost:4567/blogs`);
+
   return (
     <div>
       <SidebarProvider defaultOpen={false}>
         <Sidebar>
+          <SidebarHeader>
+            <Link to="/">
+              <div className="flex items-center gap-4 p-4 text-fuchsia-600">
+                <BookOpen size={45} absoluteStrokeWidth />
+                <div className="flex flex-col">
+                  <h2 className="text-lg font-semibold">Blog Application</h2>
+                  <span className="-mt-1 text-sm text-fuchsia-800">
+                    # better than nothing
+                  </span>
+                </div>
+              </div>
+            </Link>
+          </SidebarHeader>
           <SidebarContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton>
-                  <BookText />
-                  <Link to="/blogs">Blogs</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
+            <Collapsible
+              key={"blogs"}
+              title={"blogs"}
+              defaultOpen
+              className="group/collapsible"
+            >
+              <SidebarGroup>
+                <SidebarGroupLabel>
+                  <CollapsibleTrigger className="flex w-full justify-between align-middle text-sm">
+                    Blogs
+                    <ChevronRight
+                      size={15}
+                      className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-90"
+                    />
+                  </CollapsibleTrigger>
+                </SidebarGroupLabel>
+                <SidebarGroupContent>
+                  <CollapsibleContent>
+                    <SidebarMenu>
+                      {blogs?.map((blog) => (
+                        <SidebarMenuItem>
+                          <SidebarMenuButton>
+                            <Link to={`/blogs/${blog.id}`}>{blog.name}</Link>
+                          </SidebarMenuButton>
+                        </SidebarMenuItem>
+                      ))}
+                    </SidebarMenu>
+                  </CollapsibleContent>
+                </SidebarGroupContent>
+              </SidebarGroup>
+            </Collapsible>
           </SidebarContent>
         </Sidebar>
         <SidebarInset>
