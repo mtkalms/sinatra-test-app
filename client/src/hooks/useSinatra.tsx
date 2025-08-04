bc93b31ae423a5e3cd3fc188d4ee260ae71f9de9
Matthias Kalms <mtkalms@gmail.com>
2025-08-04 16:43:04 +0000

    Fetch on url change

--- client/src/hooks/useSinatra.tsx
+++ client/src/hooks/useSinatra.tsx
@@ -9,23 +9,27 @@ export default function useSinatra(url: string): {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<Error | null>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const result = await response.text();
-        setData(result);
-      } catch (error) {
-        setError(error as any);
-      } finally {
-        setLoading(false);
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
-    };
+      const result = await response.text();
+      setData(result);
+    } catch (error) {
+      setError(error as any);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  function load() {
     fetchData();
-  }, []); // Empty dependency array ensures it runs only once on mount
+  }
+
+  useEffect(load, []);
+  useEffect(load, [url]);
 
   return {
     data,
