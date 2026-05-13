aa979c525ce7229c481ade04425d30b21f68ae0f
Matthias Kalms <mkalms@bccfe.ca>
2026-05-13 18:46:44 +0000

    Add css loader

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,6 +17,10 @@ module.exports = (env, argv) => {
           exclude: /node_modules/,
           use: 'babel-loader',
         },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
       ],
     },
     resolve: {
