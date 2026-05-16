e451189591d9ce4a252401d0ab6705d52ed12008
Matthias Kalms <mtkalms@gmail.com>
2026-05-16 06:40:31 +0000

    Fix devcontainer for macos

--- .devcontainer/install_ruby.sh
+++ .devcontainer/install_ruby.sh
@@ -10,17 +10,31 @@ apt-get -y install \
     bash-completion \
     git \
     libaio-dev \
+    nodejs \
+    npm \
     rvm \
     unzip \
     wget
 
-wget -o wget.log https://download.oracle.com/otn_software/linux/instantclient/2340000/instantclient-basic-linux.x64-23.4.0.24.05.zip
-wget -a wget.log https://download.oracle.com/otn_software/linux/instantclient/2340000/instantclient-sdk-linux.x64-23.4.0.24.05.zip
-wget -a wget.log https://download.oracle.com/otn_software/linux/instantclient/2340000/instantclient-sqlplus-linux.x64-23.4.0.24.05.zip
+ARCH=$(uname -m)
 
-unzip instantclient-basic-linux.x64-*.zip -d /opt/oracle/
-unzip -o instantclient-sdk-linux.x64-*.zip -d /opt/oracle/
-unzip -o instantclient-sqlplus-linux.x64-*.zip -d /opt/oracle/
+if [[ "$ARCH" == "x86_64" ]]; then
+    wget -o wget.log https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-basic-linux.x64-23.8.0.25.04.zip
+    wget -a wget.log https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sdk-linux.x64-23.8.0.25.04.zip
+    wget -a wget.log https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sqlplus-linux.x64-23.8.0.25.04.zip
+
+    unzip instantclient-basic-linux.x64-*.zip -d /opt/oracle/
+    unzip -o instantclient-sdk-linux.x64-*.zip -d /opt/oracle/
+    unzip -o instantclient-sqlplus-linux.x64-*.zip -d /opt/oracle/
+elif [[ "$ARCH" == "arm*" || "$ARCH" == "aarch64" ]]; then
+    wget -o wget.log https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-basic-linux.arm64-23.8.0.25.04.zip
+    wget -a wget.log https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sdk-linux.arm64-23.8.0.25.04.zip
+    wget -a wget.log https://download.oracle.com/otn_software/linux/instantclient/2380000/instantclient-sqlplus-linux.arm64-23.8.0.25.04.zip
+
+    unzip instantclient-basic-linux.arm64-*.zip -d /opt/oracle/
+    unzip -o instantclient-sdk-linux.arm64-*.zip -d /opt/oracle/
+    unzip -o instantclient-sqlplus-linux.arm64-*.zip -d /opt/oracle/
+fi
 
 rm instantclient-*.zip wget.log
 
@@ -31,6 +45,8 @@ rvm install 3.3.6
 rvm rvmrc warning ignore /opt/project/Gemfile
 gem install bundler -v "2.5.23"
 bundle install
+
+npm install
 
 rake db:migrate
 rake db:dev_data
